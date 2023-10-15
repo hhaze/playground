357c3963d1695dcefe152d8404b17f873030cd97
jhgu <jhgu.dev@gmail.com>
2023-10-15 19:07:09 +0900

    Modify contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,43 +7,27 @@ export default () => {
 
       <h3>Language</h3>
       <ul className="list pl0">
-        <li>
-          <a className="link dim black" href="https://graphql.org/" target="_blank">GraphQL</a>
-        </li>
-        <ul className="list pl3">
-          <li>
-            <a className="link dim black" href="https://www.howtographql.com/" target="_blank">How To GraphQL</a>
-          </li>
-        </ul>
-        <li>
-          <a className="link dim black" href="https://golang.org/" target="_blank">Go</a>
-        </li>
-        <li>
-          <a className="link dim black" href="https://www.typescriptlang.org/" target="_blank">Typescript</a>
-        </li>
+        <li><a className="link dim black" href="https://www.typescriptlang.org/" target="_blank">Typescript</a></li>
+        <li><a className="link dim black" href="https://golang.org/" target="_blank">Go</a></li>
+        <li><a className="link dim black" href="https://graphql.org/" target="_blank">GraphQL</a></li>
+      </ul>
 
-        <h3>DB</h3>
-        <ul className="list pl0">
-          <li>
-            <a className="link dim black" href="https://www.mongodb.com/" target="_blank">MongoDB</a>
-          </li>
-          <li>
-            <a className="link dim black" href="https://aws.amazon.com/ko/dynamodb/" target="_blank">DynamoDB</a>
-          </li>
-          <li>
-            <a className="link dim black" href="https://www.postgresql.org/" target="_blank">PostgreSQL</a>
-          </li>
-        </ul>
+      <h3>DB</h3>
+      <ul className="list pl0">
+        <li><a className="link dim black" href="https://www.mongodb.com/" target="_blank">MongoDB</a></li>
+        <li><a className="link dim black" href="https://www.postgresql.org/" target="_blank">PostgreSQL</a></li>
+      </ul>
 
-        <h3>Framework</h3>
-        <ul className="list pl0">
-          <li>
-            <a className="link dim black" href="https://ko.reactjs.org/" target="_blank">React</a>
-          </li>
-          <li>
-            <a className="link dim black" href="https://nextjs.org/" target="_blank">Next.js</a>
-          </li>
-        </ul>
+      <h3>Framework</h3>
+      <ul className="list pl0">
+        <li><a className="link dim black" href="https://nestjs.com/" target="_blank">Nest.js</a></li>
+        <li><a className="link dim black" href="https://nextjs.org/" target="_blank">Next.js</a></li>
+        <li><a className="link dim black" href="https://ko.reactjs.org/" target="_blank">React</a></li>
+      </ul>
+
+      <h3>Other</h3>
+      <ul className="list pl0">
+        <li><a className="link dim black" href="https://bevyengine.org/" target="_blank">Bevy Engine</a></li>
       </ul>
     </div>
   )
